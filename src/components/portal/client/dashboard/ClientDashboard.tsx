af3eb80bd0fa8a76e0d481cfe92cff3679679e9e
octalve <243811625+Octalve@users.noreply.github.com>
2026-05-23 14:06:17 +0100

    Split client screens into modular entry components

---
+++ src/components/portal/client/dashboard/ClientDashboard.tsx
@@ -0,0 +1,3 @@
+"use client";
+
+export { ClientDashboard } from "../../ClientScreensLegacy";
---
+++ src/components/portal/client/phases/ClientPhases.tsx
@@ -0,0 +1,6 @@
+"use client";
+
+export {
+  ClientPhases,
+  ClientPhaseDetail,
+} from "../../ClientScreensLegacy";
